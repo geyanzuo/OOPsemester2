891ca8972683e82e1c1ae3bba30283eccb39de55
YANZUO GE <102146016@student.swin.edu.au>
2018-08-06 11:15:35 +1000

    inital commit

---
+++ EXE week2/EXE week2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE_week2
+{
+    class Program
+    {
+        static bool IsPrime(int n)
+        {
+            if (n > 1)
+            {
+                return Enumerable.Range(1, n).Where(x => n % x == 0)
+                                 .SequenceEqual(new[] { 1, n });
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+
+            int i, count, f1 = 0, f2 = 1, f3 = 0;
+            Console.Write("Enter the Limit : ");
+            count = int.Parse(Console.ReadLine());
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+            for (i = 0; i <= count; i++)
+            {
+                f3 = f1 + f2;
+                if( IsPrime(f3))
+         Console.WriteLine(f3);
+            
+                f1 = f2;
+                f2 = f3;
+            }
+
+           
+                
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
